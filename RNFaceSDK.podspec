3ff4a5d2bd84ee569e8d277a9ed9619f859aa131
Jenkins <jenkins@regula.by>
2025-05-14 11:28:14 +0300

    7.1.266-beta

--- RNFaceSDK.podspec
+++ RNFaceSDK.podspec
@@ -5,7 +5,7 @@ source = File.join(__dir__, 'ios')
 
 Pod::Spec.new do |s|
   s.name         = 'RNFaceSDK'
-  s.version      = '7.1.265-beta'
+  s.version      = '7.1.266-beta'
   s.summary      = package['description']
   s.license      = package['license']
 
--- examples/react_native/scripts/setup.sh
+++ examples/react_native/scripts/setup.sh
@@ -3,3 +3,8 @@ set -e
 
 npm install
 expo prebuild
+
+# add aaptOptions for offline match
+if ! grep -q faceSdkResource android/app/build.gradle && grep -q face-core-match package.json; then
+  sed -i '' "s/androidResources/aaptOptions {\n\t\tnoCompress 'Regula\/faceSdkResource.dat'\n\t}\n\tandroidResources/" android/app/build.gradle
+fi
